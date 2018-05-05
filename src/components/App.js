07f30b7a7336666e5b450ce967ee256753827842
ebcrowder <ebcrowder@gmail.com>
2018-05-05 09:52:12 -0700

    discogs api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
 
 import Navbar from '../components/Navbar';
 import Searchbar from '../components/Searchbar';
 
-const App = () => {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <div className="container">
-        <Searchbar />
-      </div>
-    </React.Fragment>
-  );
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      albums: []
+    };
+
+    this.albumSearch('nirvana');
+  }
+
+  albumSearch(term) {
+    axios
+      .get(`https://api.discogs.com/database/search?q=${term}`, {
+        headers: {
+          Authorization:
+            'Discogs token=clPIVQsErYhHxjjxlqJfcrWYYnIYDOMZOGdkAnsE'
+        }
+      })
+      .then(albums => this.setState({ albums: albums }));
+  }
+
+  render() {
+    const albumSearch = _.debounce(term => {
+      this.albumSearch(term);
+    }, 300);
+
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Searchbar onSearchTermChange={albumSearch} />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
+import '../styles/Searchbar.css';
 
 export default class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
-            aria-describedby="basic-addon2"
-          />
-          <div className="input-group-append">
-            <button className="btn btn-outline-secondary" type="button">
-              Button
-            </button>
+      <div className="search-bar">
+        <div className="row">
+          <div className="col-6 col-md-4" />
+          <div className="col-6 col-md-4">
+            <div className="input-group mb-3">
+              <input
+                value={this.state.term}
+                onChange={event => this.onInputChange(event.target.value)}
+              />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">
+                  Search
+                </button>
+              </div>
+            </div>
           </div>
+          <div className="col-6 col-md-4" />
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
