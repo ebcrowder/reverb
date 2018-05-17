d53d23b58c983217d61ef1ed74b42f58b7541cfc
ebcrowder <ebcrowder@gmail.com>
2018-05-16 22:09:53 -0700

    api call rendering

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../styles/Albums.css';
 
 const Albums = albums => {
   return albums.albums.map(
     records =>
       records.type === 'release' ? (
-        <div key={records.id}>
+        <div className="container" key={records.id}>
           <img src={records.cover_image} alt={records.type} />
           <p>{records.type}</p>
           <p>{records.title}</p>
         </div>
       ) : records.type === 'master' ? (
-        <div key={records.id}>
+        <div className="container" key={records.id}>
           <img src={records.cover_image} alt={records.type} />
           <p>{records.type}</p>
           <p>{records.title}</p>
