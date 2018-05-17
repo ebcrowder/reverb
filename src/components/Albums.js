87f9560cadafc91d244a6c0005f6920210e03c6a
ebcrowder <ebcrowder@gmail.com>
2018-05-16 17:36:56 -0700

    api call rendering

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
 const Albums = albums => {
-  // return Object.keys(albums).map((keyName, keyIndex) => {
-  //   return albums[keyName].data;
-  // });
+  const albumData = albums.albums.data;
+  console.log(albumData);
+
+  function toArray(obj) {
+    const result = [];
+    for (const prop in obj) {
+      const value = obj[prop];
+      if (typeof value === 'object') {
+        result.push(toArray(value)); // <- recursive call
+      } else {
+        result.push(value);
+      }
+    }
+    console.log(result);
+  }
+  toArray(albumData);
+
   return [];
 };
 
