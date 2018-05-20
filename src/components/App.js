e336e4cceb49e6ff8e00635141cf1770e9a88c65
ebcrowder <ebcrowder@gmail.com>
2018-05-20 09:34:20 -0700

    heroku test deploy

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
     };
 
     this.allResults = [];
-    this.albumSearch('');
+    this.albumSearch('father john misty');
   }
 
   albumSearch(term, nextUrl) {
